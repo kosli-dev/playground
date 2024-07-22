99277361e67a5ab82b3543bb14691d719af14118
JonJagger <jon@kosli.com>
2024-07-22 18:22:04 +0100

    Use a default port

--- config/up.sh
+++ config/up.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeu
 
-readonly PORT="${ALPHA_PORT}"
+readonly PORT="${ALPHA_PORT:-80}"
 readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export RUBYOPT='-W2'
